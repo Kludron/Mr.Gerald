302e547a6d73aa62d2f649413009bd689a306496
Kludron <kludron@lukewarmsecurityinfo.com>
2022-10-09 13:38:27 +1100

    added basic mr. gerald

---
+++ src/soul.py
@@ -0,0 +1,55 @@
+from calendar import month
+import os
+import discord
+import dotenv
+from datetime import datetime
+
+dotenv.load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+CMD_PREFIX = "$mg"
+
+questions = [q.strip() for q in open('data/qotd.list', 'r').readlines()]
+questions.reverse()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith(CMD_PREFIX):
+        await run_command(message.channel, message.author, message.content)
+    elif "hi gerald" in message.content.lower():
+        await message.channel.send(f"Hi there {message.author.nick}!")
+
+@client.event
+async def on_scheduled_event_create(event):
+    pass
+
+async def run_command(channel: discord.Message.channel, author: discord.Message.author, content: discord.Message.content):
+    cmd = content.lstrip(CMD_PREFIX).strip()
+    print(f"Command: {cmd}")
+    if cmd == "hi":
+        await channel.send(f"Hello {author.nick}!")
+    elif cmd == "qotd":
+        start = datetime(day=9, month=10, year=2022)
+
+        try:
+            question = questions[(start - datetime.now()).days]
+        except IndexError:
+            question = "I'm all out of questions!"
+
+        print(f"Sending QOTD: {question}")
+        await channel.send(question)
+    else:
+        await channel.send("I don't know that command...")
+
+client.run(TOKEN)
